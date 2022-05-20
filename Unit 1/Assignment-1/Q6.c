d622a15cf0489f342bd2130e5a40567800c08f48
Kedar Salunkhe <92195955+k3d4R2@users.noreply.github.com>
2022-05-20 07:14:56 +0530

    Add files via upload

---
+++ Unit 1/Assignment-1/Q6.c
@@ -0,0 +1,13 @@
+//
+// Created by kedar on 5/19/22.
+//
+#include <stdio.h>
+int main()
+{
+    int num1,num2,result;
+    num1=250;
+    num2=175;
+    result=num1| num2;
+    printf("result is=%d",result);
+    return 0;
+}
