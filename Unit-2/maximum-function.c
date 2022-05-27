a0e530d6b83ab5acf23961bdda4486a2b7bf86cd
Kedar Salunkhe <92195955+k3d4R2@users.noreply.github.com>
2022-05-27 21:16:20 +0530

    Add files via upload

---
+++ Unit-2/maximum-function.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int maximum(int x, int y, int z);
+int main(void)
+{
+    int number1;
+    int number2;
+    int number3;
+    printf("%s", "Enter three integers: ");
+    scanf("%d %d %d",&a,&b,&c);
+
+}
+int maximum(int x,int y,int z);
+{
+    sum = x+y+z;
+    return sum;
+}
