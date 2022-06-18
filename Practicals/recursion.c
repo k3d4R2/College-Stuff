f48ad5848667251c8bd415eb80b9b8da782b4543
Kedar Salunkhe <92195955+k3d4R2@users.noreply.github.com>
2022-06-18 18:42:30 +0530

    Add files via upload

---
+++ Practicals/recursion.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+long int factorial(int n);
+int main()
+{
+    int num;
+    printf("Enter number: ");
+    scanf("%d",&num);
+    printf("The factorial is: %ld",factorial(num));
+    return 0;
+}
+long int factorial(int n)
+{
+    if(n>=1)
+    {
+        return n
+    }
+}
+
+
