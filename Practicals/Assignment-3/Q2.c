d9955aef2f3b39e75d24d27deb3e62a8abe0279f
Kedar Salunkhe <kedarsalunkhe325@gmail.com>
2022-06-23 08:22:36 +0530

    Cmake

---
+++ Practicals/Assignment-3/Q2.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main(){
+    int unit;
+    float charge,extraCharge=0, amt,total_amt;
+
+    printf("\nEnter the unit consumed by customer:");
+    scanf("%d",&unit);
+
+    if (unit < 200 )
+    {
+        charge = 0.50;
+        extraCharge = 0;
+        amt = unit * charge;
+        total_amt = amt+extraCharge;
+    }
+    else if (unit >= 200 && unit < 400)
+    {
+        charge = 0.65;
+        extraCharge = 100;
+        amt = unit * charge;
+        total_amt = amt+extraCharge;
+    }
+    else if (unit >= 400 && unit < 600)
+    {
+        charge = 0.85;
+        extraCharge = 230;
+        amt = unit * charge;
+        total_amt = amt+extraCharge;
+    }
+    else
+    {
+        charge = 1.00;
+        extraCharge = 390;
+        amt = unit * charge;
+        total_amt = amt+extraCharge;
+    }
+
+    printf("Unit Consumed: %d\n", unit);
+    printf("Amount Charges per unit: %.2f\n",charge,amt);
+    printf("Total Charges: %.2f\n",total_amt);
+
+}
