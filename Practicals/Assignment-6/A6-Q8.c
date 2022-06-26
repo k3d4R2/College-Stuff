37a83ff49c6c5b9e4f515fad5c284566f112b0db
Kedar Salunkhe <kedarsalunkhe325@gmail.com>
2022-06-26 08:13:09 +0530

    Add Q8

---
+++ Practicals/Assignment-6/A6-Q8.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+int main()
+{
+    int i,arr[10],pos[10],neg[10],zero[10];
+    int poscount=0,negcount=0,zerocount=0;
+    printf("Enter 10 integers: ");
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("The array is: ");
+    for(i=0;i<10;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    for(i=0;i<10;i++)
+    {
+        if(arr[i]>0)
+        {
+            pos[poscount]=arr[i];
+            poscount++;
+        }
+        if(arr[i]<0)
+        {
+            neg[negcount]=arr[i];
+            negcount++;
+        }
+        if(arr[i]==0)
+        {
+            zero[zerocount]=arr[i];
+        }
+    }
+    printf("\nThe array of positive elements is: ");
+    for(i=0;i<poscount;i++)
+    {
+        printf("%d ",pos[i]);
+    }
+    printf("\nThe array of negative number is: ");
+    for (i=0;i<negcount;i++)
+    {
+        printf("%d ",neg[i]);
+    }
+    printf("\nThe array of zero elements is: ");
+    for(i=0;i<zerocount;i++)
+    {
+        printf("%d ",zero[i]);
+    }
+}
