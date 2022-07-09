bb123ca5feebca4f433d50a90d54b12098e1663c
Kedar Salunkhe <kedarsalunkhe325@gmail.com>
2022-07-09 08:14:40 +0530

    add Q2

---
+++ Practicals/Assignment-9/A9-Q2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str1[100],str2[100];
+    int i,n;
+
+    printf("Enter the string: ");
+    scanf("%s",str1);
+    n = strlen(str1);
+    //Reverse string
+    for(i=0;i<n;i++)
+    {
+        str2[i] = str1[n-1-i];
+    }
+    int k = strcmp ( str1,str2); //Comparing with reverse
+    if(k==0)
+    {
+        printf("The string is palindrome");
+    }
+    else
+    {
+        printf("Not palindrome");
+    }
+}
+
