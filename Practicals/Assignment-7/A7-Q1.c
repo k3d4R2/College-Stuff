eae364e1aca2d5e88c0bd89e494e266396f38c0e
Kedar Salunkhe <92195955+k3d4R2@users.noreply.github.com>
2022-07-18 10:19:02 +0530

    Shorter code by Avdhut

--- Practicals/Assignment-7/A7-Q1.c
+++ Practicals/Assignment-7/A7-Q1.c
@@ -2,56 +2,29 @@
 
 int main()
 {
-        int votes[6] = { 0, 0, 0, 0, 0, 0 }, cand = 0, i;
-        char keepgoing = 'y';
-while (keepgoing == 'y') 
+int votes[6] = {0, 0, 0, 0, 0, 0}, voters, cand = 0, i;
+printf("Enter number of voters: ");
+scanf("%d", &voters);
+while (voters > 0)
 {
-printf("Enter your candidate:");
-scanf("%d", &cand);
-switch (cand)
-{
-case 1:
-printf("\nEnter the candidate 1 votes:");
-scanf("%d", &votes[0]);
-votes[0] += votes[0];
-break;
-case 2:
-printf("\nEnter the candidate 2 votes:");
-scanf("%d", &votes[1]);
-votes[1] += votes[1];
-break;
-case 3:
-printf("\nEnter the candidate 3 votes:");
-scanf("%d", &votes[2]);
-votes[2] += votes[2];
-break;
-case 4:
-printf("\nEnter the candidate 4 votes:");
-scanf("%d", &votes[3]);
-votes[3] += votes[3];
-break;
-case 5:
-printf("\nEnter the candidate 5 votes:");
-scanf("%d", &votes[4]);
-votes[4] += votes[4];
-break;
-default:
-printf("\nBallot spoilt.");
-printf("\nEnter the spoilt ballot votes:");
-scanf("%d", &votes[5]);
-votes[5] += votes[5];
+printf("Type your vote : ");
+scanf("%d",&cand);
+if(cand<6 && cand>0){
+votes[cand]++;
+}else{
+votes[0]++;
 }
-printf("\nDo you want to enter more votes?:");
-getchar();
-scanf("%c", &keepgoing);
+voters--;
 }
 printf("The votes are:");
-for (i = 0; i < 6; i++) {
-if (i == 5) {
-printf("\nThe spoilt ballot votes are: %d", votes[5]);
-break;
+for (i = 0; i < 6; i++)
+{
+if (i == 0)
+{
+printf("\n spoilt ballot: %d", votes[0]);
+}else{
+printf("\n The votes for candidate %d are: %d ", i, votes[i]);
 }
-printf("\n The votes for candidate %d are: %d", i+1, votes[i]);
 }
 return 0;
-}
+}`
