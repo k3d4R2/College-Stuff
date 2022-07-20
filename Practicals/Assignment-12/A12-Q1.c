5606cef09418e866e4f13d52c49323b244867873
Kedar Salunkhe <kedarsalunkhe325@gmail.com>
2022-07-20 23:08:40 +0530

    add Q1

--- Practicals/Assignment-12/A12-Q1.c
+++ Practicals/Assignment-12/A12-Q1.c
@@ -17,20 +17,28 @@ int main()
     fprintf(fptr,"%d",num);  //Writes the number into the file
     fclose(fptr);
 
+    //writing into a file using fprintf()
     printf("Enter number to append\n");
     fptr = fopen("/home/shirou/Desktop/hello.txt","a");
     scanf("%d",&num);
     fprintf(fptr,"%d",num);  //Writes the number into the file
     fclose(fptr);
 
+    //append string using fgets()
     fptr = fopen("/home/shirou/Desktop/hello.txt","w");
     fputs("Hello World",fptr);
     fseek(fptr,6,SEEK_SET);
     fputs(" India",fptr);
     fclose(fptr);
 
+    //write character using fputc()
+    fptr = fopen("/home/shirou/Desktop/hello.txt","w");
+    fputc('H',fptr);
+    fclose(fptr);
 
-
-
+    fptr = fopen("/home/shirou/Desktop/hello.txt","r");
+    char ch;
+    ch = fgetc(fptr);
+    printf("The character read is: %c",ch);
     return 0;
 }
